d8648f0f0d78ce70017f874b8380302d69b15427
deschdev <94028614+deschdev@users.noreply.github.com>
2022-02-12 20:34:54 -0500

    additional dimension
    added an array of stars to give further dimension

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -30,11 +31,25 @@ const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 scene.add(lightHelper, gridHelper);
 
+const controls = new OrbitControls( camera, renderer.domElement );
+
+function addingStars() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( {  color: 0xffffff } );
+  const stars = new THREE.Mesh( geometry, material );
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread( 100 ));
+  stars.position.set(x, y, z);
+  scene.add(stars);
+}
+
+Array(600).fill().forEach(addingStars);
+
 function animate() {
   requestAnimationFrame( animate );
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+  controls.update();
   renderer.render( scene, camera );
 }
 
